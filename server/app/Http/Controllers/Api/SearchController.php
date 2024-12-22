4f68ce95b5deee01ec6cd1c273cdfa6d0806a2aa
pitash <pitashkumar03@gmail.com>
2024-12-22 15:45:36 +0600

    Change some code for search api

--- server/app/Http/Controllers/Api/SearchController.php
+++ server/app/Http/Controllers/Api/SearchController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Category;
 use App\Models\Country;
 use App\Models\Gallery;
 use Illuminate\Http\Request;
@@ -15,29 +14,27 @@ class SearchController extends Controller
     {
         $query = $request->input('search');
 
-        $galleries = Gallery::query()
-            ->where('title', 'like', '%' . $query . '%')
-            ->where('is_active', true)
-            ->select('id', 'title', 'description','image_path','country_id')
-            ->get();
-
-        $categories = Category::query()
-            ->where('name', 'like', '%' . $query . '%')
-            ->where('is_active', true)
-            ->select('id', 'name')
-            // ->limit(5)
-            ->get();
-
         $countries = Country::query()
-            ->where('name', 'like', '%' . $query . '%')
+            ->where('name', 'like',  $query . '%')
             ->where('is_active', true)
-            ->select('id', 'name')
+            ->pluck('id');
+
+        $galleries = Gallery::query()
+            ->where(function ($q) use ($query, $countries) {
+                $q->where('title', 'like', value:  $query . '%')
+                  ->orWhere('description', 'like', '%' . $query . '%');
+
+                if ($countries->isNotEmpty()) {
+                    $q->orWhereIn('country_id', $countries);
+                }
+            })
+            ->where('is_active', true)
+            // ->with('country:id,name')
+            ->select('id', 'title', 'description', 'image_path', 'country_id')
             ->get();
 
         return response()->json([
             'galleries' => $galleries,
-            // 'categories' => $categories,
-            // 'countries' => $countries,
         ]);
 
     }
